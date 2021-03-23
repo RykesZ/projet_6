d318b00c693147fbe42cccf6a291aff98fa788b9
RykesZ <adfourretout@hotmail.fr>
2021-03-23 17:06:59 +0100

    auth par l'utilisateur editor

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const userRoutes = require('./routes/user');
 
 
 const app = express();
-mongoose.connect('mongodb+srv://rykesz:babar01@cluster0.fzdqy.mongodb.net/SoPekocko?retryWrites=true&w=majority',
+mongoose.connect('mongodb+srv://dbeditoruser:babar01@cluster0.fzdqy.mongodb.net/SoPekocko?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
